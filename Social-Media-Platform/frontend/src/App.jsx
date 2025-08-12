46d6cfe21c067390dccf427a89078c808e5caf41
gunjabhagat <hk4979169@gmail.com>
2025-08-12 09:38:04 +0530

    Update App.jsx

--- Social-Media-Platform/frontend/src/App.jsx
+++ Social-Media-Platform/frontend/src/App.jsx
@@ -25,7 +25,7 @@ import Search from './pages/Search'
 import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
-export const serverUrl="https://social-media-platform-shinex.onrender.com/"
+export const serverUrl="https://social-media-platform-shinex.onrender.com"
 function App() {
    getCurrentUser()
    getSuggestedUsers()
